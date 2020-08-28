4ecdbdcad05295656b6a653566fd0eeae1c1e326
tmp64 <tmp32x2@gmail.com>
2020-08-28 21:17:08 +0700

    Scripts: Add build script

---
+++ scripts/BuildRelease.py
@@ -0,0 +1,445 @@
+import datetime
+import distutils
+import distutils.dir_util
+import hashlib
+import json
+import os
+import shutil
+import subprocess
+import sys
+import argparse
+
+
+# ---------------------------------------------
+# Platform stuff
+# ---------------------------------------------
+import zipfile
+
+
+def get_platform_type() -> str:
+    if sys.platform.startswith('linux'):
+        return 'linux'
+    elif sys.platform.startswith('win32'):
+        return 'windows'
+    else:
+        return 'unknown'
+
+
+class PlatformWindows:
+    script = None
+
+    def __init__(self, scr):
+        self.script = scr
+
+    def get_cmake_args(self):
+        args = []
+        if self.script.vs_version == '2017':
+            args.extend(['-G', 'Visual Studio 15 2017'])
+        elif self.script.vs_version == '2017_xp':
+            args.extend(['-G', 'Visual Studio 15 2017'])
+            args.extend(['-T', 'v141_xp'])
+        elif self.script.vs_version == '2019':
+            args.extend(['-G', 'Visual Studio 16 2019'])
+            args.extend(['-A', 'Win32'])
+
+        return args
+
+    def need_cmake_build_type_var(self):
+        return False
+
+    def update_bin_path(self):
+        self.script.paths.out_bin = self.script.paths.build + '/' + self.script.build_type + '/'
+
+    def get_dll_ext(self):
+        return '.dll'
+
+
+class PlatformLinux:
+    script = None
+
+    def __init__(self, scr):
+        self.script = scr
+
+    def get_cmake_args(self):
+        args = []
+        args.extend(['-G', 'Ninja'])
+        return args
+
+    def need_cmake_build_type_var(self):
+        return False
+
+    def update_bin_path(self):
+        self.script.paths.out_bin = self.script.paths.build + '/'
+
+    def get_dll_ext(self):
+        return '.so'
+
+
+# ---------------------------------------------
+# Targets
+# ---------------------------------------------
+class FileToCopy:
+    src = ''
+    dst = ''
+
+    def __init__(self, _src, _dst):
+        self.src = _src
+        self.dst = _dst
+
+
+COMMON_FILES_TO_COPY = [
+    FileToCopy('README.md', 'README.md')
+]
+
+
+class TargetClient:
+    script = None
+
+    def __init__(self, scr):
+        self.script = scr
+
+    def get_build_target_names(self):
+        return ['client']
+
+    def get_file_list(self):
+        files = COMMON_FILES_TO_COPY
+        files.append(FileToCopy(self.script.paths.out_bin + 'client' + self.script.platform.get_dll_ext(),
+                                'valve_addon/cl_dlls/client' + self.script.platform.get_dll_ext()))
+        files.append(FileToCopy('gamedir/gfx', 'valve_addon/gfx'))
+        files.append(FileToCopy('gamedir/ui', 'valve_addon/ui'))
+
+        if get_platform_type() == 'windows':
+            files.append(FileToCopy(self.script.paths.out_bin + 'client.pdb',
+                                    'valve_addon/cl_dlls/client.pdb'))
+
+        return files
+
+
+class TargetServer:
+    script = None
+
+    def __init__(self, scr):
+        self.script = scr
+
+    def get_build_target_names(self):
+        return ['hl']
+
+    def get_file_list(self):
+        files = COMMON_FILES_TO_COPY
+        files.append(FileToCopy(self.script.paths.out_bin + 'hl' + self.script.platform.get_dll_ext(),
+                                'valve_addon/dlls/hl' + self.script.platform.get_dll_ext()))
+
+        if get_platform_type() == 'windows':
+            files.append(FileToCopy(self.script.paths.out_bin + 'hl.pdb',
+                                    'valve_addon/dlls/hl.pdb'))
+
+        return files
+
+
+# ---------------------------------------------
+# Build script class
+# ---------------------------------------------
+class BuildScript:
+    class Paths:
+        base = ''
+        build = ''
+        out_bin = ''
+        archive_root = ''
+        archive_files = ''
+
+    DEFAULT_VERSION = [0, 1, 0, 'dev', '']
+    allowed_targets = ['client', 'server']
+    allowed_build_types = ['debug', 'release']
+    allowed_vs_versions = ['2017', '2017_xp', '2019']
+
+    platform = None
+    build_target = None
+    build_target_name = ''
+    build_type = ''  # Values are Debug or RelWithDebInfo
+    vs_version = ''
+    release_version = ''
+
+    cmake_binary = ''
+
+    repo_root = ''
+    git_hash = ''
+    date_code = ''
+    out_dir_name = ''
+    paths = Paths()
+
+    def run(self):
+        if get_platform_type() == 'unknown':
+            print("Unknown platform. This script only supports Windows and Linux.")
+            exit(1)
+        elif get_platform_type() == 'windows':
+            self.platform = PlatformWindows(self)
+        elif get_platform_type() == 'linux':
+            self.platform = PlatformLinux(self)
+
+        try:
+            self.repo_root = \
+                subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[
+                    0].rstrip().decode('utf-8').replace('\\', '/') + '/'
+            self.git_hash = \
+                subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE).communicate()[
+                    0].rstrip().decode('utf-8')
+        except Exception as e:
+            print('Failed to get info from Git repo: ' + str(e))
+            exit(1)
+
+        self.date_code = datetime.datetime.now().strftime('%Y%m%d')
+
+        self.parse_arguments()
+
+        self.run_cmake()
+        self.build_targets()
+        self.copy_files()
+        self.create_install_metadata()
+        self.create_zip()
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description='Builds production build of BugfixedHL, ready for release on '
+                                                     'GitHub.')
+
+        parser.add_argument('--target', action='store', required=True, choices=self.allowed_targets,
+                            help='(required) target to build')
+        parser.add_argument('--build-type', action='store', required=True, choices=self.allowed_build_types,
+                            help='(required) build type')
+        parser.add_argument('--vs', action='store', required=(get_platform_type() == 'windows'),
+                            choices=self.allowed_vs_versions, help='(windows only, required) Visual Studio version')
+        # parser.add_argument('--updater', action='store_true',
+        #                    help='enable updater')
+        parser.add_argument('--out-dir', action='store',
+                            help='output directory (if not set, uses <workdir>/BugfixedHL-<version>)')
+        parser.add_argument('--cmake-bin', action='store', default=shutil.which("cmake"),
+                            help='path to cmake binary (PATH used instead)')
+
+        # Version override
+        parser.add_argument('--v-major', action='store', type=int, default=self.DEFAULT_VERSION[0],
+                            help='sets major version number')
+        parser.add_argument('--v-minor', action='store', type=int, default=self.DEFAULT_VERSION[1],
+                            help='sets minor version number')
+        parser.add_argument('--v-patch', action='store', type=int, default=self.DEFAULT_VERSION[2],
+                            help='sets patch version number')
+        parser.add_argument('--v-tag', action='store', default=self.DEFAULT_VERSION[3],
+                            help='sets version tag')
+        parser.add_argument('--v-meta', action='store', default=self.DEFAULT_VERSION[4],
+                            help='sets version metadata')
+
+        # Parse arguments
+        args = parser.parse_args()
+        self.vs_version = args.vs
+        self.cmake_binary = args.cmake_bin
+
+        # Set target
+        self.build_target_name = args.target
+        if args.target == 'client':
+            self.build_target = TargetClient(self)
+        elif args.target == 'server':
+            self.build_target = TargetServer(self)
+
+        # Set build type
+        if args.build_type == 'debug':
+            self.build_type = 'Debug'
+        elif args.build_type == 'release':
+            self.build_type = 'RelWithDebInfo'
+
+        # Parse version
+        self.release_version = "{}.{}.{}".format(args.v_major, args.v_minor, args.v_patch)
+        if args.v_tag:
+            self.release_version = "{}-{}".format(self.release_version, args.v_tag)
+        if args.v_meta:
+            self.release_version = "{}+{}".format(self.release_version, args.v_meta)
+
+        # Set output directory
+        self.out_dir_name = 'BugfixedHL-{}-{}-{}-{}'.format(self.release_version.replace('+', '-'),
+                                                       self.build_target_name, self.git_hash, self.date_code)
+
+        out_dir = args.out_dir
+        if not out_dir:
+            work_dir = os.getcwd()
+            out_dir = self.out_dir_name
+            if os.path.exists(os.path.realpath(work_dir + '/' + out_dir)):
+                count = 1
+                while True:
+                    out_dir = '{}-{:03}'.format(self.out_dir_name, count)
+                    if os.path.exists(os.path.realpath(work_dir + '/' + out_dir)):
+                        count += 1
+                    else:
+                        break
+
+                    if count > 999:
+                        print('Failed to create out path. Try setting it manually')
+                        exit(1)
+                        break
+            self.paths.base = os.path.realpath(work_dir + '/' + out_dir)
+
+        self.paths.base = os.path.realpath(self.paths.base) + '/'
+        self.paths.build = self.paths.base + 'build'
+        self.paths.archive_root = self.paths.base + 'archive' + '/'
+        self.paths.archive_files = self.paths.archive_root + self.out_dir_name + '/'
+
+        try:
+            os.mkdir(self.paths.base)
+            os.mkdir(self.paths.build)
+            os.mkdir(self.paths.archive_root)
+            os.mkdir(self.paths.archive_files)
+            self.platform.update_bin_path()
+        except OSError as e:
+            print("Failed to create out paths: {}. Try setting it manually.".format(str(e)))
+            exit(1)
+
+    def run_cmake(self):
+        try:
+            args = [self.cmake_binary]
+            args.extend(['-S', self.repo_root])
+            args.extend(['-B', self.paths.build])
+            args.extend(self.platform.get_cmake_args())
+
+            if self.platform.need_cmake_build_type_var():
+                args.extend(['-DCMAKE_BUILD_TYPE=' + self.build_type])
+
+            print("---------------- Running CMake with arguments:")
+            for i in args:
+                print('\'', i, '\' ', sep='', end='')
+            print()
+            print()
+
+            subprocess.run(args, check=True)
+
+            print()
+            print()
+        except subprocess.CalledProcessError:
+            print('CMake finished with non-zero status code.')
+            exit(1)
+        except Exception as e:
+            print('Failed to run CMake: {}.'.format(str(e)))
+            exit(1)
+
+    def build_targets(self):
+        try:
+            targets = ''
+
+            for target in self.build_target.get_build_target_names():
+                targets += target + ' '
+
+            targets = targets.strip()
+
+            print("---------------- Building targets", targets)
+            args = [self.cmake_binary]
+            args.extend(['--build', self.paths.build])
+            args.extend(['--target', targets])
+            args.extend(['--config', self.build_type])
+
+            for i in args:
+                print('\'', i, '\' ', sep='', end='')
+            print()
+            print()
+
+            subprocess.run(args, check=True)
+
+            print()
+            print()
+        except subprocess.CalledProcessError:
+            print('Build finished with non-zero status code.')
+            exit(1)
+        except Exception as e:
+            print('Failed to run build: {}.'.format(str(e)))
+            exit(1)
+
+    def copy_files(self):
+        print("---------------- Copying files")
+        files = self.build_target.get_file_list()
+
+        for i in files:
+            print(i.src, '->', i.dst)
+
+            if os.path.isabs(i.src):
+                src = i.src
+            else:
+                src = self.repo_root + i.src
+            dst = self.paths.archive_files + i.dst
+
+            try:
+                if not os.path.exists(src):
+                    print('Error: file', src, "doesn't exist.")
+                    exit(1)
+
+                if os.path.exists(dst):
+                    print('Error: file', dst, 'already exists.')
+                    exit(1)
+
+                if os.path.isdir(src):
+                    distutils.dir_util.copy_tree(src, dst)
+                else:
+                    # Create parent dirs
+                    distutils.dir_util.mkpath(os.path.dirname(dst))
+
+                    # Copy file
+                    shutil.copyfile(src, dst)
+
+            except Exception as e:
+                print('Failed to copy files: {}.'.format(str(e)))
+                exit(1)
+
+        print()
+        print()
+
+    def create_install_metadata(self):
+        print("---------------- Creating metadata")
+        try:
+            meta = {
+                'version': self.release_version,
+                'files': {}
+            }
+
+            for root, dirs, files in os.walk(self.paths.archive_root):
+                for file in files:
+                    fullpath = os.path.join(root, file)
+                    path = os.path.relpath(fullpath, self.paths.archive_files + 'valve_addon').replace('\\', '/')
+
+                    # Skip files outside of valve_addon
+                    if path.startswith('..'):
+                        continue
+
+                    file_data = {
+                        'size': os.path.getsize(fullpath),
+                        'hash_sha1': ''
+                    }
+
+                    hasher = hashlib.sha1()
+                    with open(fullpath, 'rb') as infile:
+                        buf = infile.read()
+                        hasher.update(buf)
+
+                    file_data['hash_sha1'] = hasher.hexdigest()
+
+                    meta['files'][path] = file_data
+
+            with open(self.paths.archive_files + 'valve_addon/bugfxiedhl_install_metadata.dat', "a") as f:
+                f.write(json.dumps(meta, sort_keys=True, indent=4))
+        except Exception as e:
+            print('Failed to create metadata file: {}.'.format(str(e)))
+            exit(1)
+
+    def create_zip(self):
+        print("---------------- Creating ZIP archive")
+        try:
+            zipf = zipfile.ZipFile('{}{}.zip'.format(self.paths.base, self.out_dir_name), 'w',
+                                   zipfile.ZIP_DEFLATED)
+
+            for root, dirs, files in os.walk(self.paths.archive_root):
+                for file in files:
+                    path = os.path.join(root, file)
+                    zipf.write(path, os.path.relpath(path, self.paths.archive_root))
+
+            zipf.close()
+        except Exception as e:
+            print('Failed to create ZIP archive: {}.'.format(str(e)))
+            exit(1)
+
+
+# Script entry point
+if __name__ == '__main__':
+    script = BuildScript()
+    script.run()

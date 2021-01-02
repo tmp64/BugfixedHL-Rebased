14b299211b97d0653e2093cf81c0863d915c631e
tmp64 <tmp32x2@gmail.com>
2021-01-02 17:03:27 +0700

    Server: Fix entity function export

--- src/game/server/util.h
+++ src/game/server/util.h
@@ -85,15 +85,9 @@ typedef int BOOL;
 // This is the glue that hooks .MAP entity class names to our CPP classes
 // The _declspec forces them to be exported by name so we can do a lookup with GetProcAddress()
 // The function is used to intialize / allocate the object for the entity
-#ifdef _WIN32
-#define LINK_ENTITY_TO_CLASS(mapClassName, DLLClassName)               \
-	extern "C" _declspec(dllexport) void mapClassName(entvars_t *pev); \
-	void mapClassName(entvars_t *pev) { GetClassPtr((DLLClassName *)pev); }
-#else
 #define LINK_ENTITY_TO_CLASS(mapClassName, DLLClassName) \
-	extern "C" void mapClassName(entvars_t *pev);        \
+	extern "C" CBASE_DLLEXPORT void mapClassName(entvars_t *pev);        \
 	void mapClassName(entvars_t *pev) { GetClassPtr((DLLClassName *)pev); }
-#endif
 
 //
 // Conversion among the three types of "entity", including identity-conversions.

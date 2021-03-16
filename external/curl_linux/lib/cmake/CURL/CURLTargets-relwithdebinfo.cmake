cb22ccccc6740b071eefa3b27482d57226f4d23a
tmp64 <tmp32x2@gmail.com>
2021-03-16 22:49:26 +0700

    Fix dependency problem with libssl-dev for CI(try number six! SIX!)

--- external/curl_linux/lib/cmake/CURL/CURLTargets-relwithdebinfo.cmake
+++ external/curl_linux/lib/cmake/CURL/CURLTargets-relwithdebinfo.cmake
@@ -1,15 +1,15 @@
 #----------------------------------------------------------------
-# Generated CMake target import file.
+# Generated CMake target import file for configuration "RelWithDebInfo".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "CURL::libcurl" for configuration ""
-set_property(TARGET CURL::libcurl APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+# Import target "CURL::libcurl" for configuration "RelWithDebInfo"
+set_property(TARGET CURL::libcurl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
 set_target_properties(CURL::libcurl PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "C"
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libcurl.a"
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libcurl.a"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS CURL::libcurl )

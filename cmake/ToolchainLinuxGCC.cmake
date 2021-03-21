28f54371ecf06408cc8394733b5f4f2d059763c2
tmp64 <tmp32x2@gmail.com>
2021-03-21 11:07:05 +0700

    Client: Fix linking with 64 bit libraries on Linux

--- cmake/ToolchainLinuxGCC.cmake
+++ cmake/ToolchainLinuxGCC.cmake
@@ -1,7 +1,7 @@
 # This toolchain adds -m32 to compiler flags
 # It is required to make CMake link with proper 32-bit libraries
 
-set( CMAKE_SYSTEM_NAME Linux )
+include( Linux32CrossCompile )
 
 set( CMAKE_C_COMPILER gcc -m32 )
 set( CMAKE_CXX_COMPILER g++ -m32) 

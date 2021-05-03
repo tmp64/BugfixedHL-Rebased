0ea2db616faccc211e5f31755ae2475b596a45d4
tmp64 <tmp32x2@gmail.com>
2021-05-03 13:43:29 +0700

    PM: Remove vec3_t

--- src/pm_shared/pm_math.h
+++ src/pm_shared/pm_math.h
@@ -2,9 +2,6 @@
 #define PM_MATH_H
 #include <mathlib/mathlib.h>
 
-// TODO: Remove when vec3_t is replaced everywhere.
-using vec3_t = Vector;
-
 void AngleVectors(const Vector &angles, Vector *forward, Vector *right, Vector *up);
 void AngleVectorsTranspose(const Vector &angles, Vector *forward, Vector *right, Vector *up);
 

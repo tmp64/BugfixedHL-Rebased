9bb069424a7effb618b856a71e4fa1495b6fc686
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2021-08-12 10:18:34 +0400

    Client: Allow thirdperson mode with sv_cheats enabled

--- src/game/client/in_camera.cpp
+++ src/game/client/in_camera.cpp
@@ -150,7 +150,10 @@ extern trace_t SV_ClipMoveToEntity(edict_t *ent, Vector start, Vector mins, Vect
 void CL_DLLEXPORT CAM_Think(void)
 {
 	//	RecClCamThink();
-	if (gEngfuncs.GetMaxClients() > 1 && CL_IsThirdPerson())
+
+	static ConVarRef sv_cheats("sv_cheats");
+
+	if (gEngfuncs.GetMaxClients() > 1 && CL_IsThirdPerson() && !sv_cheats.GetBool())
 		CAM_ToFirstPerson();
 
 	Vector origin;

5b506dc6c3b0dacb45767491f5c0a28acfdd637d
tmp64 <tmp32x2@gmail.com>
2021-11-14 11:05:02 +0700

    Bump version to v1.7.1

--- scripts/BuildRelease.py
+++ scripts/BuildRelease.py
@@ -13,7 +13,7 @@ from CreateMetadata import create_metadata
 
 
 # Also needs to be changed in CMakeLists.txt
-DEFAULT_VERSION = [1, 7, 0, 'dev', '']
+DEFAULT_VERSION = [1, 7, 1, 'dev', '']
 
 
 # ---------------------------------------------

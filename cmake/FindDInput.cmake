012ac9825ed8ecd899de0be16bc44bd7e8235828
tmp64 <tmp32x2@gmail.com>
2023-12-02 16:21:52 +0700

    Move Windows libraries to a subdirectory

--- cmake/FindDInput.cmake
+++ cmake/FindDInput.cmake
@@ -6,7 +6,7 @@ if( NOT MSVC )
 	message( FATAL_ERROR "DInput is only supported on Windows with MSVC++" )
 endif()
 
-find_library( DINPUT_LIB NAMES dinput8.lib PATHS ${CMAKE_SOURCE_DIR}/external/dinput NO_DEFAULT_PATH )
+find_library( DINPUT_LIB NAMES dinput8.lib PATHS ${CMAKE_SOURCE_DIR}/external/windows/dinput NO_DEFAULT_PATH )
 
 include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( DInput DEFAULT_MSG DINPUT_LIB )
@@ -14,7 +14,7 @@ find_package_handle_standard_args( DInput DEFAULT_MSG DINPUT_LIB )
 if( DINPUT_LIB )
 	add_library( DInput SHARED IMPORTED )
 	set_property( TARGET DInput PROPERTY IMPORTED_IMPLIB ${DINPUT_LIB} )
-	target_include_directories( DInput INTERFACE ${CMAKE_SOURCE_DIR}/external/dinput/include )
+	target_include_directories( DInput INTERFACE ${CMAKE_SOURCE_DIR}/external/windows/dinput/include )
 	target_compile_definitions( DInput INTERFACE DIRECTINPUT_VERSION=0x0800 )
 endif()
 

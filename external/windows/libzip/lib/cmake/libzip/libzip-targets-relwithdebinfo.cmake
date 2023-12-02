33e514e2592cf7f0960d9b380a1c913d4b599712
tmp64 <tmp32x2@gmail.com>
2023-12-02 18:20:49 +0700

    Update Windows libraries

--- external/windows/libzip/lib/cmake/libzip/libzip-targets-relwithdebinfo.cmake
+++ external/windows/libzip/lib/cmake/libzip/libzip-targets-relwithdebinfo.cmake
@@ -12,8 +12,8 @@ set_target_properties(libzip::zip PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/zip.lib"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS libzip::zip )
-list(APPEND _IMPORT_CHECK_FILES_FOR_libzip::zip "${_IMPORT_PREFIX}/lib/zip.lib" )
+list(APPEND _cmake_import_check_targets libzip::zip )
+list(APPEND _cmake_import_check_files_for_libzip::zip "${_IMPORT_PREFIX}/lib/zip.lib" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)

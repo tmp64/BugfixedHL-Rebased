e1f155ae4c47ca3528d801b328e12b8d8b39957a
tmp64 <tmp32x2@gmail.com>
2023-12-03 16:43:07 +0700

    Client: LoadSprite: Use res from HUD (#193)
    Resolves #193

--- src/game/client/cl_util.cpp
+++ src/game/client/cl_util.cpp
@@ -91,16 +91,8 @@ void ConsolePrint(const char *string)
 
 HSPRITE LoadSprite(const char *pszName)
 {
-	int i;
 	char sz[256];
-
-	if (ScreenWidth < 640)
-		i = 320;
-	else
-		i = 640;
-
-	sprintf(sz, pszName, i);
-
+	sprintf(sz, pszName, gHUD.m_iRes);
 	return SPR_Load(sz);
 }
 

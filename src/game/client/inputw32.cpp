5880d6535bede3d9aa5236585bd2bc7171456117
tmp64 <tmp32x2@gmail.com>
2024-08-24 14:11:38 +0700

    Client: Input: Sanitize sensitivity

--- src/game/client/inputw32.cpp
+++ src/game/client/inputw32.cpp
@@ -637,6 +637,28 @@ void IN_GetMousePos(int *mx, int *my)
 
 /*
 ===========
+IN_GetMouseSensitivity
+
+Get mouse sensitivity with sanitization
+===========
+*/
+float IN_GetMouseSensitivity()
+{
+	// Absurdly high sensitivity values can cause the game to hang, so clamp
+	if (sensitivity->value > 10000.0)
+	{
+		gEngfuncs.Cvar_SetValue(sensitivity->name, 10000.0);
+	}
+	else if (sensitivity->value < 0.01)
+	{
+		gEngfuncs.Cvar_SetValue(sensitivity->name, 0.01);
+	}
+
+	return sensitivity->value;
+}
+
+/*
+===========
 IN_ResetMouse
 
 FIXME: Call through to engine?
@@ -714,7 +736,7 @@ void IN_ScaleMouse(float *x, float *y)
 	float my = *y;
 
 	// This is the default sensitivity
-	float mouse_senstivity = (gHUD.GetSensitivity() != 0) ? gHUD.GetSensitivity() : sensitivity->value;
+	float mouse_senstivity = (gHUD.GetSensitivity() != 0) ? gHUD.GetSensitivity() : IN_GetMouseSensitivity();
 
 	// Using special accleration values
 	if (m_customaccel->value != 0)

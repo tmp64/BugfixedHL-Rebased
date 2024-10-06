04cae0e9f4819ef878a3d9238f09f464c1a1dc19
tmp64 <tmp32x2@gmail.com>
2024-10-06 14:58:11 +0700

    Set version to 1.12.0 for release

--- scripts/BuildRelease.py
+++ scripts/BuildRelease.py
@@ -13,7 +13,7 @@ from CreateMetadata import create_metadata
 
 
 # Also needs to be changed in CMakeLists.txt
-DEFAULT_VERSION = [1, 12, 0, 'dev', '']
+DEFAULT_VERSION = [1, 12, 0, '', '']
 
 
 # ---------------------------------------------
